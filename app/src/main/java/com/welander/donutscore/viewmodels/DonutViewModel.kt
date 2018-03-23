8a5ee0fa67009f6b9aa064621b8ec786f406fb65
Niklas Welander <niklas.welander@gmail.com>
2018-03-23 11:12:52 +0100

    Added mocking in viewmodel for development purposes

---
+++ app/src/main/java/com/welander/donutscore/viewmodels/DonutViewModel.kt
@@ -0,0 +1,7 @@
+package com.welander.donutscore.viewmodels
+
+/**
+ * Crafted by welander on 2018-03-23.
+ */
+class DonutViewModel {
+}
--- app/src/main/java/com/welander/donutscore/viewmodels/ScoreActivityViewModel.kt
+++ app/src/main/java/com/welander/donutscore/viewmodels/ScoreActivityViewModel.kt
@@ -6,15 +6,26 @@ import com.welander.donutscore.R
 import com.welander.donutscore.models.CreditScoreInformation
 import com.welander.donutscore.network.CreditScoreStore
 import com.welander.donutscore.utils.safePercent
+import io.reactivex.Single
 import io.reactivex.SingleObserver
 import io.reactivex.disposables.Disposable
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by welander on 2018-03-17.
  */
 
 class ScoreActivityViewModel constructor(private val creditScoreStore: CreditScoreStore) : BaseObservable() {
+
+    val errorViewModel = ErrorViewModel()
+    val domutViewModel = DonutViewModel()
+
+    /*
+    var loading
+     |- DonutViewModel
+     |- ErrorViewModel
+     */
 
     @Bindable
     var percentageValue: Int = 1 // Make sure minimum value for Pie is not 0, it disappears
@@ -51,23 +62,45 @@ class ScoreActivityViewModel constructor(private val creditScoreStore: CreditSco
     }
 
     fun fetchCreditScore() {
-        creditScoreStore.fetchCreditScoreInformation().subscribeWith(object : SingleObserver<CreditScoreInformation> {
-            override fun onSuccess(successValue: CreditScoreInformation) {
-                showLoading(false)
-                showResult(successValue)
-            }
-
-            override fun onSubscribe(d: Disposable) {
-                Timber.i("Subscribed for credit score")
-                showLoading(true)
-            }
-
-            override fun onError(e: Throwable) {
-                showLoading(false)
-                showError(e)
-            }
-
-        })
+        // creditScoreStore.fetchCreditScoreInformation().subscribeWith(creditScoreObserver)
+        mockScore()
     }
+
+    val creditScoreObserver = object : SingleObserver<CreditScoreInformation> {
+        override fun onSuccess(successValue: CreditScoreInformation) {
+            showLoading(false)
+            showResult(successValue)
+        }
+
+        override fun onSubscribe(d: Disposable) {
+            Timber.i("Subscribed for credit score")
+            showLoading(true)
+        }
+
+        override fun onError(e: Throwable) {
+            showLoading(false)
+            showError(e)
+        }
+
+    }
+
+    // Development methods
+
+    fun mockScore() {
+        Single.just(CreditScoreInformation(300, 700, 0))
+                .subscribeWith(creditScoreObserver)
+    }
+
+    fun mockLoading() {
+        Single.just(CreditScoreInformation(567, 1000, 100))
+                .delay(1, TimeUnit.HOURS)
+                .subscribeWith(creditScoreObserver)
+    }
+
+    fun mockError() {
+        val errorSingle: Single<CreditScoreInformation> = Single.just(null)
+        errorSingle.subscribeWith(creditScoreObserver)
+    }
+
 
 }
